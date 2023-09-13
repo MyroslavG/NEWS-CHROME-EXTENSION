6d426940b2568f7f7509275e61a1ca72a24c0bde
MyroslavG <realmyros@gmail.com>
2023-09-13 17:06:12 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-document.getElementById('submit').addEventListener('click', function() {
-    fetch('/get_url')
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("submit");
+
+    searchButton.addEventListener("click", function () {
+        const searchInput = document.getElementById("fname").value;
+
+        fetch('https://v74ez2mxa5npidfeyji345szfi0doufy.lambda-url.us-east-1.on.aws/', {
+            method: "POST",
+            /*headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },*/
+            body: `input=${searchInput}`, 
+        })
         .then(response => {
             console.log(response)
             return response.json();
         })
         .then(result => {
-            // Get the 'result' div element
-            const resultDiv = document.getElementById('result');
-            // Create an anchor tag to make the URL clickable
-            const anchorTag = document.createElement('a');
-            anchorTag.textContent = 'Result: ' + result.result;
-            anchorTag.href = result.result; // Set the URL as the href attribute
-            // Add the anchor tag to the 'result' div
-            resultDiv.appendChild(anchorTag);
+            const resultDiv = document.getElementById('text');
+            resultDiv.innerHTML = result;
         })
         .catch(error => {
             console.error('API request failed:', error);
         });
+    });    
 });
